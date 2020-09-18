64313e8e950dc9525de67070f2eee78433c3b5f0
Umair Farooqui <34832943+MUFAzmi@users.noreply.github.com>
2020-09-19 01:36:31 +0530

    Some Changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,8 @@ export class About extends Component {
                <h1>About this App</h1>
                <h3> Search Engine for Github Users</h3> 
                <p>This project uses the Github API to fetch user profiles and their repositories</p>
-               <h4> V:1.0.0 </h4>
+               <h4> V:2.0.0 </h4>
+               <h6>Developed By <a href="http://instagram.com/mufazmi">@MUFAzmi</a></h6>
             </div>
         )
     }
